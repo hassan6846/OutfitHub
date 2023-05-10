1c7a371da45f258e4f85056048d08854b404769f
Jager <96440779+hassan6846@users.noreply.github.com>
2023-05-10 11:26:30 -0700

    mainanyway

--- Web/server/app.js
+++ Web/server/app.js
@@ -1,9 +1,22 @@
 const express=require("express")
-const  app=express();
+const app=express();
+const port=3000;
 
+//  routes to create
+/* 
+shop fetch all the  product 
+/men
+/women
+/kids 
+/beauty 
+/girls
+/ all  add some filteratiion here
+*/
+/* 
 
-
-
-
-
-
+/user/liked  for liked products
+*/
+// listen
+app.listen(port,function(req,res){
+    console.log(`app is running on ${port}`)
+})
