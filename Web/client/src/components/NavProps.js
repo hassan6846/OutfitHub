6caa31735d26c5e0da5daaa7809e8a14754fafbd
Jager <ha6817334@gmail.com>
2023-05-24 09:19:49 -0700

    isuck

---
+++ Web/client/src/components/NavProps.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./NavProps.css"
+
+const NavProps = (imguUrl) => {
+  return (
+    <div className='propWrapper'>
+         <img src alt='Close button' className='' />
+         <p>walter</p>
+    </div>
+  )
+}
+
+export default NavProps
--- Web/client/src/components/Navbar.js
+++ Web/client/src/components/Navbar.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import "./Navbar.css"
+
 const Navbar = () => {
   return (
-    <nav>
-      <div className='main_Nav'></div>
-    </nav>
+    <div>Navbar</div>
   )
 }
 
