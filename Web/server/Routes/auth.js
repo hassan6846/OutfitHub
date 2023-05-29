dfa3d3f54a02b08f7e51ec73c0b7bb167ee5af74
Jager <ha6817334@gmail.com>
2023-05-29 10:37:43 -0700

    unnecessary
    removed some directory

--- Web/server/Routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const Router=express.Router();
-//  routers has controllers in mvc framework
-// controllers 
-
-router.post('/signup', signup );
-router.post('/signin', signin );
-router.get('/logout', logout );
