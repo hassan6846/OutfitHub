d930729a34a94c3a9247998b0d4dcbadf6edac39
Jager <ha6817334@gmail.com>
2023-06-08 06:02:37 -0700

    main

--- Web/client/src/components/FilterBar.js
+++ Web/client/src/components/FilterBar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./FilterBar.css"
 import SubCategorybtn from './SubCategorybtn'
+
 const FilterBar = () => {
   return (
     <div className='filterbar_wrapper'>
         <div className='filterBtns_Container'>
+          <SubCategorybtn/>
           <SubCategorybtn/>
           <SubCategorybtn/>
           <SubCategorybtn/>
