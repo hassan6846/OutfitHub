cd71dc7956392aa8a54f015d5ef6e57b95ca8790
hassan6846 <ha6817334@gmail.com>
2023-07-11 20:50:40 +0500

    main

--- Web/client/src/App.js
+++ Web/client/src/App.js
@@ -11,10 +11,15 @@ import SignupPage from './pages/user/Signup';
 import ErrorPage from './pages/user/ErrorPage';
 import Products from './pages/user/Products';
 import Singleproduct from './pages/user/Singleproduct';
+import ForgotPassword from './pages/user/ForgotPassword';
+import ResetPassword from './pages/user/ResetPassword';
+import Faq from './pages/user/Faq';
+
 import Otp from './pages/user/Otp';
 
 // testing components
 import ProductCard from './components/ProductCard';
+
 // import SubCategorySlider from './Layouts/SubCategorySlider/SubCategorySlider';
 
 
@@ -23,14 +28,22 @@ function App() {
 <Router>
   <Routes>
 <Route path='/' element={<Home/>} />
+<Route path='*' element={<ErrorPage/>} />
 <Route path='/shop' element={<Products/>} />
 <Route path='/login' element={<LoginPage/>} />
 <Route path='/signup' element={<SignupPage/>} />
+<Route exact path='/password/forgot' element ={<ForgotPassword/>}/>
+<Route exact path='/password/reset/:token' element={<ResetPassword/>}/>
+<Route path='/password/otp/:token' element={<Otp/>} />
 {/* 404 page  */}
-<Route path='*' element={<ErrorPage/>} />
 <Route path='/product' element={<ProductCard/>} />
-<Route path='/single' element={<Singleproduct/>} />
-<Route path='/verify' element={<Otp/>} />
+<Route path='/product/:id' element={<Singleproduct/>} />
+<Route path='/cart'/>
+{/* User Routes */}
+<Route path='/about' element={<Faq/>}/>
+
+
+
 {/* protected Routes are down below */}
   </Routes>
 </Router>
---
+++ Web/client/src/pages/user/ResetPassword.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./ResetPassword.css"
+const ResetPassword = () => {
+  return (
+    <div>ResetPassword</div>
+  )
+}
+
+export default ResetPassword
