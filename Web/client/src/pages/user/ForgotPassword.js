9e64c6a28e5f66cb757cca835d95fac86d917f1c
hassan6846 <ha6817334@gmail.com>
2023-07-13 14:37:46 +0500

    main

--- Web/client/src/pages/user/ForgotPassword.js
+++ Web/client/src/pages/user/ForgotPassword.js
@@ -1,20 +1,55 @@
-import React from 'react'
-import "./ForgotPassword.css"
-import { Link } from "react-router-dom"
-import Footer from "../../Layouts/footer/Footer"
+import React from 'react';
+import "./ForgotPassword.css";
+import { useFormik } from 'formik';
+import { Link } from "react-router-dom";
+import Footer from "../../Layouts/footer/Footer";
+import toast, { Toaster } from 'react-hot-toast';
+
 const ForgotPassword = () => {
+  const formik = useFormik({
+    initialValues: {     
+      email: '',
+    },
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: (values) => {
+      if (!isValidEmail(values.email)) {
+        toast.error('Kindly fill all the fields');
+      } else {
+        // Perform password recovery logic here
+        // Display success message or navigate to another page
+      }
+    },
+  });
+
+  const isValidEmail = (email) => {
+    // Simple email validation regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
   return (
     <div>
-    <section className='Forgot_wrapper'>
-  <Link className='logo_link' to="/"> <img alt='mainlogo' src='https://svgshare.com/i/vGt.svg' title='loogo'/></Link>
- <div className='forgor-form'>
-  <h3 className='forgor-head'>Forgot Password </h3>
-  <p className='forgor-tag'>No Worries,We'll send you instruction in your email</p>
- </div>
-    </section>
-  <Footer/>
-  </div>
-  )
-}
+      <section className='Forgot_wrapper'>
+        <Link className='logo_link' to="/">
+          <img alt='mainlogo' src='https://svgshare.com/i/vGt.svg' title='logo' />
+        </Link>
+        <form onSubmit={formik.handleSubmit} className='forgor-form'>
+          <h3 className='forgor-head'>Forgot Password</h3>
+          <p className='forgor-tag'>No Worries,We'll send you instructions in your email</p>
+          <input
+            {...formik.getFieldProps("email")}
+            type='email'
+            placeholder='Email'
+            className='forgor-inp'
+          />
+          <button className='forgor-btn' type="submit">Recover Password</button>
+        </form>
+      </section>
+      <Footer/>
+      <Toaster /> {/* React-hot-toast component */}
+    </div>
+  );
+};
 
-export default ForgotPassword
+export default ForgotPassword;
--- Web/client/src/pages/user/ResetPassword.js
+++ Web/client/src/pages/user/ResetPassword.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import "./ResetPassword.css"
+import {Link} from "react-router-dom"
+import Footer from "../../Layouts/footer/Footer";
 const ResetPassword = () => {
   return (
-    <div>ResetPassword</div>
+    <div>
+      <section className='reset-wrapper'>
+      <Link className='logo_link' to="/">
+          <img alt='mainlogo' src='https://svgshare.com/i/vGt.svg' title='logo' />
+        </Link>
+        <form method='put' className='reset-Passform'>
+       
+         <h3 className='reset-head'>Enter New Password</h3>
+          <input className='reset-inp'  type='password' placeholder='New Password'/>
+          <input  className='reset-inp' type='password' placeholder='Repeat Password'/>
+          <button type="submit" className='forgor-btn'>Update Password</button>
+        </form>
+      </section>
+      <Footer/>
+    </div>
   )
 }
 
