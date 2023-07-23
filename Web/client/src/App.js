66efbc51d9fd536d0d35f09d87465448e4f23ad7
hassan6846 <ha6817334@gmail.com>
2023-07-23 21:49:15 +0500

    created breadcrumb

--- Web/client/src/App.js
+++ Web/client/src/App.js
@@ -1,10 +1,18 @@
-// Liabrary Or Modules
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import axios from "axios"
+import axios from "axios";
 import WebFont from "webfontloader";
+import AdminSidebar from './pages/admin/Sidebar/AdminSidebar';
+import Overview from './pages/admin/Components/Overview';
+import AllProducts from './pages/admin/Components/AllProducts';
+import AddProduct from './pages/admin/Components/AddProduct';
+import AllUsers from './pages/admin/Components/AllUsers';
+import Orders from './pages/admin/Components/Orders';
+import Messages from './pages/admin/Components/Messages';
+import AdminLogout from './pages/admin/Components/AdminLogout';
+
 /**
- * Pages 
+ * Pages
  */
 import Home from './pages/user/Home';
 import LoginPage from './pages/user/Login';
@@ -15,66 +23,58 @@ import Singleproduct from './pages/user/Singleproduct';
 import ForgotPassword from './pages/user/ForgotPassword';
 import ResetPassword from './pages/user/ResetPassword';
 import Faq from './pages/user/Faq';
-
 import Otp from './pages/user/Otp';
-
-// testing components
-import ProductCard from './components/ProductCard';
-import AdminSidebar from './pages/admin/Sidebar/AdminSidebar';
-import AllProducts from './pages/admin/Components/AllProducts';
-import AddProduct from './pages/admin/Components/AddProduct';
-import AllUsers from './pages/admin/Components/AllUsers';
-import Orders from './pages/admin/Components/Orders';
-import Messages from './pages/admin/Components/Messages';
-import AdminLogout from './pages/admin/Components/AdminLogout';
-// import SubCategorySlider from './Layouts/SubCategorySlider/SubCategorySlider';
-
 
 function App() {
   const [stripeApiKey, setStripeApiKey] = useState("");
-  //get api key for payment 
-  async function GetApiKey(){
- const {data}=await axios.get("/api/v1/stripeapikey")
- setStripeApiKey(data.stripeApiKey)
+
+  // get api key for payment
+  async function getApiKey() {
+    const { data } = await axios.get("/api/v1/stripeapikey");
+    setStripeApiKey(data.stripeApiKey);
   }
-  // benifits of loading fonts in app.js
-  useEffect(()=>{
+
+  // benefits of loading fonts in app.js
+  useEffect(() => {
     WebFont.load({
-      google:{
+      google: {
         families: ["Roboto", "Droid Sans", "Chilanka"],
-      }})
-  })
+      },
+    });
+  }, []);
+
   return (
-<Router>
-  <Routes>
-<Route exact={true} path='/' element={<Home/>} />
-<Route path='*' element={<ErrorPage/>} />
-<Route path='/shop' element={<Products/>} />
-<Route path='/shop/men'/>
-<Route path='/login' element={<LoginPage/>} />
-<Route path='/signup' element={<SignupPage/>} />
-<Route exact path='/password/forgot' element ={<ForgotPassword/>}/>
-<Route exact path='/password/reset/:token' element={<ResetPassword/>}/>
-<Route path='/password/otp/:token' element={<Otp/>} />
-{/* 404 page  */}
-<Route path='/product' element={<ProductCard/>} />
-<Route path='/product/:id' element={<Singleproduct/>} />
-<Route path='/cart'/>
-{/* User Routes */}
-<Route path='/about' element={<Faq/>}/>
-{/* protected Routes are down below */}
-<Route exact={true} path='/admin' element={<AdminSidebar/>} />
-<Route path='/admin/products' element={<AllProducts/>} />
-<Route path='/admin/products/all' element={<AddProduct/>} />
-<Route path='/admin/users' element={<AllUsers/>}/>
-<Route path='/admin/orders' element={<Orders/>}/>
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route exact path='/password/forgot' element={<ForgotPassword />} />
+        <Route exact path='/password/reset/:token' element={<ResetPassword />} />
+        <Route path='/password/otp/:token' element={<Otp />} />
 
-<Route path='/admin/messages' element={<Messages/>}/>
-<Route path='/admin/logout' element={<AdminLogout/>}/>
-  </Routes>
-</Router>
+        {/* 404 page */}
+        <Route path='*' element={<ErrorPage />} />
 
-  )
+        {/* Protected Routes */}
+        <Route path='/admin/*' element={<AdminSidebar />}>
+          <Route index element={<Overview />} />
+          <Route path='products' element={<AllProducts />} />
+          <Route path='products/add' element={<AddProduct />} />
+          <Route path='users' element={<AllUsers />} />
+          <Route path='orders' element={<Orders />} />
+          <Route path='messages' element={<Messages />} />
+          <Route path='logout' element={<AdminLogout />} />
+        </Route>
+
+        {/* User Routes */}
+        <Route path='/shop' element={<Products />} />
+        <Route path='/product/:id' element={<Singleproduct />} />
+        <Route path='/about' element={<Faq />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- Web/client/src/pages/admin/Sidebar/AdminSidebar.js
+++ Web/client/src/pages/admin/Sidebar/AdminSidebar.js
@@ -1,54 +1,66 @@
 import React from 'react';
-import { Switch, Link, Routes, Route } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import { ImSwitch } from "react-icons/im";
-import { BsGraphUpArrow, BsFillBagFill } from "react-icons/bs"
-import { BiMessageSquareDetail } from "react-icons/bi"
-import { AiFillFileAdd,AiOutlineArrowLeft } from "react-icons/ai"
-import { HiOutlineUsers } from "react-icons/hi"
-import { MdOutlinePendingActions } from "react-icons/md"
-// css
+import { BsGraphUpArrow, BsFillBagFill } from "react-icons/bs";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { AiFillFileAdd, AiOutlineArrowLeft } from "react-icons/ai";
+import { HiOutlineUsers } from "react-icons/hi";
+import { MdOutlinePendingActions } from "react-icons/md";
 import './AdminSidebar.css';
-import AllUsers from '../Components/AllUsers';
-import AddProduct from '../Components/AddProduct';
-import Overview from "../Components/Overview"
-import Messages from "../Components/Messages"
-import AllProducts from "../Components/AllProducts"
+
 const AdminSidebar = () => {
+  const location = useLocation();
+
+  // Extract the pathname from the location object
+  const { pathname } = location;
+
+  // Convert the pathname into a breadcrumb format
+  // e.g., '/admin/products/all' -> ['admin', 'products', 'all']
+  const pathParts = pathname.split('/').filter((part) => part !== '');
 
   return (
-
     <div className='Admin_wrapper'>
       <div className='Action-Sidebar'>
         <Link to='/' className='login-logo logo_admin'>
-          <AiOutlineArrowLeft className='arrow'/>  <img alt='company' src='./logo.svg' />
+          <AiOutlineArrowLeft className='arrow' /> <img alt='company' src='./logo.svg' />
         </Link>
         <Link className='Switch_links' to='/admin'>
-          <BsGraphUpArrow />  Overview
+          <BsGraphUpArrow /> Overview
         </Link>
         <Link className='Switch_links' to='/admin/products'>
-          <BsFillBagFill />     Products
+          <BsFillBagFill /> Products
         </Link>
-        <Link className='Switch_links' to='/admin/products/all'>
-          <AiFillFileAdd />  Add New Product
+        <Link className='Switch_links' to='/admin/products/add'>
+          <AiFillFileAdd /> Add New Product
         </Link>
         <Link className='Switch_links' to='/admin/users'>
-          <HiOutlineUsers />        All Users
+          <HiOutlineUsers /> All Users
         </Link>
         {/* Add other links here */}
         <Link className='Switch_links' to='/admin/orders'>
-          <MdOutlinePendingActions />   Orders
+          <MdOutlinePendingActions /> Orders
         </Link>
         <Link className='Switch_links' to='/admin/messages'>
-          <BiMessageSquareDetail />     Message
+          <BiMessageSquareDetail /> Message
         </Link>
         <Link className='logout-btn' to='/admin/logout'>
           <ImSwitch /> Logout
         </Link>
       </div>
+
       <div className='admin-content'>
+        <div className='admin-breadCrumb admin-breadcrumb'>
+          {/* Render breadcrumb links */}
+          {pathParts.map((part, index) => (
+            <React.Fragment key={index}>
+              <Link to={`/${pathParts.slice(0, index + 1).join('/')}`}>{part}</Link>
+              {index < pathParts.length - 1 && <span className='breadcrumb-separator'> / </span>}
+            </React.Fragment>
+          ))}
+        </div>
+        <Outlet />
       </div>
     </div>
-
   );
 };
 
