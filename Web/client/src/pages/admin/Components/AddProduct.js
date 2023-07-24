4eca9ab87de65574fc1ca4d484ce0f1e7bcd19fb
hassan6846 <ha6817334@gmail.com>
2023-07-25 00:39:16 +0500

    Update AddProduct.js

--- Web/client/src/pages/admin/Components/AddProduct.js
+++ Web/client/src/pages/admin/Components/AddProduct.js
@@ -2,7 +2,12 @@ import React from 'react'
 import "./AddProduct.css"
 const AddProduct = () => {
   return (
-    <div className='addproduct-wrapper'>AddProduct</div>
+    <div className='addproduct-wrapper'>
+      <div>
+        <label for="inp" >ProductName</label><br/>
+        <input  name='inp' placeholder='Product Name'/>
+      </div>
+    </div>
   )
 }
 
