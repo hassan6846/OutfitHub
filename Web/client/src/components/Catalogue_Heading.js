e5e328bb2c35c5a70a309c99be9929cc89f19cdf
hassan6846 <ha6817334@gmail.com>
2023-08-05 16:25:15 +0500

    main

--- Web/client/src/components/Catalogue_Heading.js
+++ Web/client/src/components/Catalogue_Heading.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import "./Trending_heading.css";
 import React from 'react'
-
+import {FcNext} from "react-icons/fc"
 const Cathead = (props) => {
   return (
     <div className="heading__flex_wrapper_90">
         <div className="heading__child_70">
             <h3 className="cat_heading_text" >{props.heading}</h3>
-           <Link to={props.LinkPage}  className="Nav_link" >{props.LinkText}</Link>
+           <Link to={props.LinkPage}  className="Nav_link" >{props.LinkText} <FcNext  className="next_icon" /> </Link>
         </div>
     </div>
   )
--- Web/client/src/pages/user/Products.js
+++ Web/client/src/pages/user/Products.js
@@ -1,37 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './products.css';
-import { motion, AnimatePresence } from 'framer-motion'; // Import motion and AnimatePresence
+
 import Nav from '../../Layouts/NavbarMain/ResponsiveNav';
 import Footer from '../../Layouts/footer/Footer';
 import CategoryBar from '../../components/CategoryBar';
-import Card from "../../components/ProductCard"
-import ProductCard from '../../components/ProductCard';
+
 const Products = () => {
-  const [isSidebarToggled, setIsSidebarToggled] = useState(false);
-
-  const handleSidebarToggle = () => {
-    setIsSidebarToggled((prevValue) => !prevValue);
-  };
-
+  
   return (
     <div className='product-page-wrapper'>
       <Nav />
       <CategoryBar />
-      <div className='parental-row_product'>
-        <AnimatePresence>
-          {/* Wrap the sidebar with the motion.div */}
-          <motion.div
-            className={`sidebar_product ${isSidebarToggled ? 'toggled' : ''}`}
-            initial={{ width: '25%' }}
-            animate={{ width: isSidebarToggled ? '2%' : '25%' }}
-            exit={{ width: '25%' }}
-            transition={{ duration: 0.3 }} // Adjust the duration as needed
-          >
-            <button onClick={handleSidebarToggle}>Toggle</button>
-          </motion.div>
-        </AnimatePresence>
-        <div className='product_container_cards'><ProductCard/></div>
-      </div>
+    
+    
       <Footer />
     </div>
   );
