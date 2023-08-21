98a59c2ba2b80aa4f529a1dd5ad9708e81d2868d
hassan6846 <ha6817334@gmail.com>
2023-08-21 20:35:03 +0500

    cleaning-up

--- Web/client/src/store.js
+++ Web/client/src/store.js
@@ -1,3 +1,7 @@
- import thunk from "redux-thunk";
-import { applyMiddleware,combineReducers,legacy_createStore,compose } from "redux";
-// combine all reducers below
+// src/store.js
+import { legacy_createStore } from "redux";
+import reducer from "./reducer";
+
+const store = legacy_createStore(reducer);
+
+export default store;
