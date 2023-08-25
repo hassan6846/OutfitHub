6cc0949468c244285193408538480ac8b99f1e8f
hassan6846 <ha6817334@gmail.com>
2023-08-25 12:08:25 +0500

    main

--- Web/client/src/Layouts/footer/Footer.js
+++ Web/client/src/Layouts/footer/Footer.js
@@ -1,32 +1,9 @@
-import React from "react";
+import React from 'react'
 
-import "./Footer.css";
-
-const Footer = (props) => {
+const Footer = () => {
   return (
-    <footer id="footer" style={{marginTop:props.MarginTop}}>
-      <div className="leftFooter">
-        <h4>DOWNLOAD OUR APP</h4>
-        <p>Download App for Android and IOS mobile phone</p>
-        <img className="store_image"  src="./playstore.png" alt="playstore" />
-        <img className="store_image" src="./Appstore.png" alt="Appstore" />
-      </div>
+    <div>Footer</div>
+  )
+}
 
-      <div className="midFooter">
-        <h1>DESIGN.</h1>
-        <p> We care about you, not about the sale</p>
-
-        <p>Copyrights 2023 &copy; Hassan Ali</p>
-      </div>
-
-      <div className="rightFooter">
-        <h4>Follow Us</h4>
-        <a  target="__blank"  href="http://instagram.com/jessepinkman">Instagram</a>
-        <a target="__blank"  href="http://youtube.com/waltersus">Youtube</a>
-        <a  target="__blank" href="http://instagram.com/Solong_gay_bowser">Facebook</a>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
+export default Footer
