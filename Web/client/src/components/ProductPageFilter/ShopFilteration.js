4de7dad86ff77c7bd9babd5dd6d96f2c1ef7bc51
hassan6846 <96440779+hassan6846@users.noreply.github.com>
2023-09-11 12:44:25 +0500

    main

--- Web/client/src/components/ProductPageFilter/ShopFilteration.js
+++ Web/client/src/components/ProductPageFilter/ShopFilteration.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import "./ShopFilteration.css"
+import { Link } from 'react-router-dom'
+import {BiSlider} from "react-icons/bi"
+import {BsChevronDown} from "react-icons/bs"
 const ShopFilteration = () => {
   return (
-    <div>ShopFilteration</div>
+    <div className='shopfilter_wrapper'>
+      <div className='filter_shopBtn'>
+      <Link className='filter_Link_2'>Sort <BsChevronDown/> </Link>
+
+        <Link className='filter_Link_2'>Price Range <BsChevronDown style={{fontSize:"13px"}}/> </Link>
+        <Link className='filter_Link'>Filter <BiSlider/> </Link>
+
+
+      </div>
+    </div>
   )
 }
 
