7ecbff4d017e5f662a4a6e2390b046f8bb8a1310
hassan6846 <96440779+hassan6846@users.noreply.github.com>
2023-09-25 22:03:53 +0500

    main

--- Web/client/src/pages/user/UserProfile/Contact.js
+++ Web/client/src/pages/user/UserProfile/Contact.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import {MDBBtn, MDBInput,MDBTextArea } from "mdb-react-ui-kit"
+import { MDBBtn, MDBInput, MDBTextArea } from "mdb-react-ui-kit"
 import "./Contact.css"
 const Contact = () => {
   return (
     <div className='container_CONTACT'>
-    <p className='contact_us'>Contact Us.</p>
-    <p className='contact_us_sub'>Feel Free to Contact Us.</p>
-    <div className='contact_inputs_form'>
-    <MDBInput label='Name'  type='text' size='lg' />
-    <MDBInput label='Email'  type='text' size='lg' />
-    <MDBInput label='Subject' placeholder='Title/Subject'  type='text' size='lg' />
-    <MDBTextArea placeholder='Please Dont Enter Links Here Just Reason or Problem You are facing' label='Message' id='textAreaExample' rows={10} />
-    <MDBBtn style={{backgroundColor:"#4BB497",color:"white"}}>Submit</MDBBtn>
-    </div>
+      <p className='contact_us'>Contact Us.</p>
+      <p className='contact_us_sub'>Feel Free to Contact Us.</p>
+      <div className='contact_inputs_form'>
+        <MDBInput label='Name' type='text' size='lg' />
+        <MDBInput label='Email' type='text' size='lg' />
+        <MDBInput label='Subject' placeholder='Title/Subject' type='text' size='lg' />
+        <MDBTextArea placeholder='Please Dont Enter Links Here Just type The Reason or Problem You are facing' label='Message' id='textAreaExample' rows={10} />
+        <MDBBtn style={{ backgroundColor: "#4BB497", color: "white" }}>Submit</MDBBtn>
+      </div>
     </div>
   )
 }
--- Web/client/src/pages/user/UserProfile/Vendor.js
+++ Web/client/src/pages/user/UserProfile/Vendor.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import "./Vendor.css"
 const Vendor = () => {
   return (
-    <div>Vendor</div>
+    <>
+    <p className='user_vendor_p'>Apply For Membership</p>
+    <p className='vendor_note'>Note: Kindly fill the form completely. Otherwise, your application will be rejected.</p>
+</>
   )
 }
 
