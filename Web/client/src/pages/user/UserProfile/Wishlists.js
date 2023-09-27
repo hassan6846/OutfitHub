8fc60af7506f7a8cc85163de153f067e88ba8727
hassan6846 <96440779+hassan6846@users.noreply.github.com>
2023-09-27 20:19:26 +0500

    main

--- Web/client/src/pages/user/UserProfile/Wishlists.js
+++ Web/client/src/pages/user/UserProfile/Wishlists.js
@@ -1,9 +1,116 @@
-import React from 'react'
+import React from 'react';
+import { MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
 import "./Wishlists.css"
-const Wishlists = () => {
+export default function Wishlists() {
   return (
-    <>Wishlists</>
-  )
-}
+    <>
+    <p className='wishlist_page_head'>Wishlists</p>
+    <MDBTable align='middle'>
+      <MDBTableHead>
+        <tr>
+          <th scope='col'>ProductName</th>
+          <th scope='col'>Unit Price</th>
+          <th scope='col'>Status</th>
+          <th scope='col'>Position</th>
+          <th scope='col'>Actions</th>
+        </tr>
+      </MDBTableHead>
+      <MDBTableBody>
+        <tr>
+          <td>
+            <div className='d-flex align-items-center'>
+              <img
+                src='https://mdbootstrap.com/img/new/avatars/8.jpg'
+                alt=''
+                style={{ width: '45px', height: '45px',borderRadius:'0px'}}
+                className='rounded-circle img_wishlists'
+              />
+              <div className='ms-3'>
+                <p className='fw-bold mb-1'>John Doe</p>
+                <p className='text-muted mb-0'>john.doe@gmail.com</p>
+              </div>
+            </div>
+          </td>
+          <td>
+          
+            <p className='text-muted mb-0'>10$ <span style={{ textDecoration: "line-through" }}>20$</span></p>
+          </td>
+          <td>
+            <MDBBadge color='success' light pill>
+              In Stock
+            </MDBBadge>
+          </td>
+          <td>Senior</td>
+          <td>
+            <MDBBtn color='link' rounded size='sm'>
+              Edit
+            </MDBBtn>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div className='d-flex align-items-center'>
+              <img
+                src='https://mdbootstrap.com/img/new/avatars/6.jpg'
+                alt=''
+                style={{ width: '45px', height: '45px' }}
+                className='rounded-circle'
+              />
+              <div className='ms-3'>
+                <p className='fw-bold mb-1'>Alex Ray</p>
+                <p className='text-muted mb-0'>alex.ray@gmail.com</p>
+              </div>
+            </div>
+          </td>
+          <td>
 
-export default Wishlists
+            <p className='text-muted mb-0'>10$ <span style={{ textDecoration: "line-through" }}>20$</span></p>
+          </td>
+          <td>
+            <MDBBadge color='secondary' light pill>
+              Out Of stock
+            </MDBBadge>
+          </td>
+          <td>Junior</td>
+          <td>
+            <MDBBtn color='link' rounded size='sm'>
+              Edit
+            </MDBBtn>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div className='d-flex align-items-center'>
+              <img
+                src='https://mdbootstrap.com/img/new/avatars/7.jpg'
+                alt=''
+                style={{ width: '45px', height: '45px' }}
+                className='rounded-circle'
+              />
+              <div className='ms-3'>
+                <p className='fw-bold mb-1'>Kate Hunington</p>
+                <p className='text-muted mb-0'>kate.hunington@gmail.com</p>
+              </div>
+            </div>
+          </td>
+          <td>
+ 
+ 
+            <p className='text-muted mb-0'>10$ <span style={{ textDecoration: "line-through" }}>20$</span></p>
+          </td>
+          <td>
+            <MDBBadge color='success' pill light>
+              In Stock
+            </MDBBadge>
+          </td>
+          <td>Senior</td>
+          <td>
+            <MDBBtn color='link' rounded size='sm'>
+              Edit
+            </MDBBtn>
+          </td>
+        </tr>
+      </MDBTableBody>
+    </MDBTable></>
+  );
+}
