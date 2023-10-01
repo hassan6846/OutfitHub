f250901700bf8ad9fceeccc593401235498b6bf5
hassan6846 <96440779+hassan6846@users.noreply.github.com>
2023-10-01 13:31:18 +0500

    main

--- Web/client/src/pages/user/UserProfile/EditUserProfile.js
+++ Web/client/src/pages/user/UserProfile/EditUserProfile.js
@@ -1,8 +1,56 @@
 import React from 'react'
 
+import "./EditUserProfile.css"
+import { MDBInput } from "mdb-react-ui-kit"
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+import { FiEdit2 } from 'react-icons/fi';
+import { GrFormAdd } from "react-icons/gr"
+import { defaultUserImg } from '../../../helpers/GlobalVariables';
+
 const EditUserProfile = () => {
   return (
-    <div>EditUserProfile</div>
+    <div className='user_profile_container'>
+
+      <p className='wishlist_page_head'>EditUserProfile</p>
+      <div className='img_flex'> <img style={{ height: "80px", marginBottom: "1rem", borderRadius: "60%" }} src={defaultUserImg} alt="user_img" />  </div>
+      <div style={{ columnGap: "1rem" }} className='user_detail_wrapper'>
+        {/* Childs of Grid Elements */}
+        <div>
+          <h3 className='user_name'>Full Name  </h3>
+          <p className='user_name_ans'><MDBInput size='lg' placeholder='Full Name' /></p>
+        </div>
+
+        {/* 2 */}
+        <div>
+          <h3 className='user_name'>Email</h3>
+          <p style={{ display: "flex", alignItems: "center", columnGap: "1rem" }} className='user_name_ans'>ha6817334@gmail.com <span className='update_email'><Link style={{ color: "white" }}><FiEdit2 /></Link> </span></p>
+        </div>
+        {/* 3 */}
+        <div>
+          <h3 className='user_name'>Gender  </h3>
+          <p className='user_name_ans'>
+            <Form.Select aria-label="Default select example">
+              <option>Select Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </Form.Select>
+          </p>
+        </div>
+        {/* 4 */}
+        <div className='details_wrapper'>
+          <h3 className='user_name'>Mobile Number</h3>
+          <p style={{ display: "flex", alignItems: 'center', columnGap: "0.2rem" }} className='user_name_ans'>03160490149 <span style={{ backgroundColor: "rgb(75,180,151)", color: "white", padding: "4px 8px", borderRadius: "50px" }}> <Link style={{ color: "white" }}><FiEdit2 /></Link> </span>  </p>
+        </div>
+        {/* 5 */}
+        <div className='details_wrapper'>
+          <h3 className='user_name'>Address</h3>
+          <p style={{ display: "flex", alignItems: 'center', columnGap: "0.2rem" }} className='update_profile_style'>03160490149 <span style={{ backgroundColor: "rgb(75,180,151)", color: "white", padding: "4px 8px", borderRadius: "50px" }}> <Link style={{ color: "white" }}><GrFormAdd style={{ fontSize: "1rem" }} /></Link> </span>  </p>
+        </div>
+        {/* ENDS OF CHILDS */}
+      </div>
+      <button className='btn_update_profilee'>Save</button>
+    </div>
   )
 }
 
--- Web/client/src/pages/user/UserProfile/ProfileOverView.js
+++ Web/client/src/pages/user/UserProfile/ProfileOverView.js
@@ -1,56 +1,57 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import "./ProfileOverView.css"
-import {FiEdit2} from "react-icons/fi"
-import {defaultUserImg} from "../../../helpers/GlobalVariables"
+import { FiEdit2 } from "react-icons/fi"
+import { defaultUserImg } from "../../../helpers/GlobalVariables"
 
 const ProfileOverView = () => {
   return (
-    <> 
-   
-    <p className='wishlist_page_head'>Manage Your Account,</p>
-    <div className='user_profile_container'>
-   <div className='img_flex'> <img style={{height:"80px",marginBottom:"1rem",borderRadius:"60%"}} src={defaultUserImg} alt="user_img" /> <input placeholder='upload' type="file" id="file" title='Select Image maxiumum 2mb' className='image_upload_ui'/>  </div>
-      <div className='user_detail_wrapper'>
-        <div>
-          <h3 className='user_name'>Full Name |<span><Link to="/user/edit">Edit</Link></span> </h3>
-          <p className='user_name_ans'>HassanAli</p>
+    <>
+
+      <p className='wishlist_page_head'>Manage Your Account,</p>
+      <div className='user_profile_container'>
+        <div className='img_flex'> <img style={{ height: "80px", marginBottom: "1rem", borderRadius: "60%" }} src={defaultUserImg} alt="user_img" />  </div>
+        {/* MAIN */}
+        <div className='user_detail_wrapper'>
+          <div>
+            <h3 className='user_name'>Full Name |<span><Link to="/user/edit">Edit</Link></span> </h3>
+            <p className='user_name_ans'>HassanAli</p>
+          </div>
+          {/* Email */}
+          <div>
+            <h3 className='user_name'>Email</h3>
+            <p style={{ display: "flex", alignItems: "center", columnGap: "1rem" }} className='user_name_ans'>ha6817334@gmail.com <span className='update_email'><Link style={{ color: "white" }}><FiEdit2 /></Link> </span></p>
+          </div>
+          {/* GENDER */}
+          <div>
+            <h3 className='user_name'>Gender</h3>
+            <p className='user_name_ans'>Male 👨</p>
+          </div>
+          {/* DATE OF Joined */}
+          <div>
+            <h3 className='user_name'>Joined In</h3>
+            <p className='user_name_ans'>25/2/2023</p>
+          </div>
+          {/* Orders */}
+          <div>
+            <h3 className='user_name'>Orders</h3>
+            <p className='user_name_ans'>3 </p>
+          </div>
+          {/* SUS */}
+          <div className='details_wrapper'>
+            <h3 className='user_name'>Cart | <span><Link to="/cart">Checkout</Link></span></h3>
+            <p className='user_name_ans'> 0</p>
+          </div>
+          {/* Mobile Number */}
+          <div className='details_wrapper'>
+            <h3 className='user_name'>Mobile Number</h3>
+            <p style={{ display: "flex", alignItems: 'center', columnGap: "1rem" }} className='user_name_ans'>03160490149 <span style={{ backgroundColor: "rgb(75,180,151)", color: "white", padding: "4px 8px", borderRadius: "50px" }}> <Link style={{ color: "white" }}><FiEdit2 /></Link> </span>  </p>
+          </div>
+
         </div>
-        {/* Email */}
-        <div>
-          <h3 className='user_name'>Email</h3>
-          <p className='user_name_ans'>ha6817334@gmail.com</p>
-        </div>
-        {/* GENDER */}
-        <div>
-          <h3 className='user_name'>Gender</h3>
-          <p className='user_name_ans'>Male 👨</p>
-        </div>
-        {/* DATE OF Joined */}
-        <div>
-          <h3 className='user_name'>Joined In</h3>
-          <p className='user_name_ans'>25/2/2023</p>
-        </div>
-        {/* Orders */}
-        <div>
-          <h3 className='user_name'>Orders</h3>
-          <p className='user_name_ans'>3 </p>
-        </div>
-        {/* SUS */}
-        <div className='details_wrapper'>
-          <h3 className='user_name'>Cart | <span><Link to="/cart">Checkout</Link></span></h3>
-          <p className='user_name_ans'> 0</p>
-        </div>
-        {/* Mobile Number */}
-        <div className='details_wrapper'>
-          <h3 className='user_name'>Mobile Number</h3>
-          <p className='user_name_ans'>03160490149 </p>
-        </div>
-     
-      </div>
-   <div className='user_action_container'>   <Link to="/user/edit" className='wishlistLink' style={{display:"flex",alignItems:"center",width:"fit-content",columnGap:'0.3rem'}}>Edit Profile <FiEdit2/></Link> 
-      <Link to="/password/forgot" className='wishlistLink' style={{display:"flex",alignItems:"center",width:"fit-content",columnGap:'0.3rem'}}>Update Password </Link></div>
-    </div></>
+        <div className='user_action_container'>   <Link to="/user/edit" className='wishlistLink' style={{ display: "flex", alignItems: "center", width: "fit-content", columnGap: '0.3rem' }}>Edit Profile <FiEdit2 /></Link>
+          <Link to="/password/forgot" className='wishlistLink' style={{ display: "flex", alignItems: "center", width: "fit-content", columnGap: '0.3rem' }}>Update Password </Link></div>
+      </div></>
   )
 }
 
