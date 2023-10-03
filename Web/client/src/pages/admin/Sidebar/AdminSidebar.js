1b11b6624a56d425d05bb760630b362ea8992179
hassan6846 <96440779+hassan6846@users.noreply.github.com>
2023-10-03 23:30:22 +0500

    Update AdminSidebar.js

--- Web/client/src/pages/admin/Sidebar/AdminSidebar.js
+++ Web/client/src/pages/admin/Sidebar/AdminSidebar.js
@@ -1,52 +1,83 @@
-import { React, useState } from 'react'
-import { Link } from "react-router-dom"
-import {  BiUpload, BiLogOutCircle } from "react-icons/bi"
-import {TfiStatsUp} from "react-icons/tfi"
-import { MdOutlinePendingActions, MdOutlineKeyboardArrowLeft } from "react-icons/md"
-import { defaultUserImg } from '../../../helpers/GlobalVariables'
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import { BiUpload, BiLogOutCircle } from "react-icons/bi";
+import { TfiStatsUp } from "react-icons/tfi";
+import {
+  MdOutlinePendingActions,
+  MdOutlineKeyboardArrowLeft,
+} from "react-icons/md";
+import { defaultUserImg } from "../../../helpers/GlobalVariables";
 // css
-import "./AdminSidebar.css"
+import "./AdminSidebar.css";
 
 const AdminSidebar = () => {
-    const [toggled, setToggled] = useState(true)
-    function SidebarTOGGLE() {
-        setToggled(!toggled)
-    }
+  const [toggled, setToggled] = useState(true);
+  function SidebarTOGGLE() {
+    setToggled(!toggled);
+  }
 
-    // classes
-    const divClassName = toggled ? "user_profile_aside_toggle" : "user_profile_aside"
-    const Asideheading = toggled ? "aside_heading_sidebar_toggled" : "aside_heading_sidebar"
-    const Asidespan = toggled ? "icon_text_aside_toggled" : "icon_text_aside"
-    const Aside_img = toggled ? "aside_user_profile_img_toggle" : "aside_user_profile_img"
-    const aside_icon_state = toggled ? "aside_icon_state_toggled" : "aside_icon_state"
+  // classes
+  const divClassName = toggled
+    ? "user_profile_aside_toggle"
+    : "user_profile_aside";
+  const Asideheading = toggled
+    ? "aside_heading_sidebar_toggled"
+    : "aside_heading_sidebar";
+  const Asidespan = toggled ? "icon_text_aside_toggled" : "icon_text_aside";
+  const Aside_img = toggled
+    ? "aside_user_profile_img_toggle"
+    : "aside_user_profile_img";
+  const aside_icon_state = toggled
+    ? "aside_icon_state_toggled"
+    : "aside_icon_state";
 
-    return (
-        <aside className={divClassName}>
-            <div onClick={SidebarTOGGLE} className='aside_user_toggle'><MdOutlineKeyboardArrowLeft className='aside_icon_toggle' /></div>
-            {/* user Profile IMAGE */}
-            <div className='aside_profiel_image_cover'> <img className={Aside_img} src={defaultUserImg} alt="profile_img" /> <BiUpload className='upload_icon_aside' />   </div>
-            {/* IMAGE DIV ENDS HERE */}
-            <div className='aside_link_flex'>
-                <p className={Asideheading}>Overview</p>
-                <Link to="/admin" className='aside_links'> <TfiStatsUp className={aside_icon_state} /> <span className={Asidespan}>Overview</span> </Link>
-            </div>
-            {/* ORDER */}
-            <div className='aside_link_flex'>
-             <p className={Asideheading}>Orders</p>
-             <Link className='aside_links' to="/user/order"> <MdOutlinePendingActions className={aside_icon_state} /><span className={Asidespan}> My Orders</span></Link>
-            </div>
-            {/* WISHLISTS */}
-            {/* SETTINGS */}
-            {/* vender OPTION REQUEST */}
-            {/* Contact US */}
-            <div className='aside_link_flex'>
-             <p className={Asideheading}>Actions</p>
-             <Link to="/logout" className='aside_links'> <BiLogOutCircle className={aside_icon_state} /> <span className={Asidespan}>Logout</span> </Link>
-            </div>
+  return (
+    <aside className={divClassName}>
+      <div onClick={SidebarTOGGLE} className="aside_user_toggle">
+        <MdOutlineKeyboardArrowLeft className="aside_icon_toggle" />
+      </div>
+      {/* user Profile IMAGE */}
+      <div className="aside_profiel_image_cover">
+        {" "}
+        <img
+          className={Aside_img}
+          src={defaultUserImg}
+          alt="profile_img"
+        />{" "}
+        <BiUpload className="upload_icon_aside" />{" "}
+      </div>
+      {/* IMAGE DIV ENDS HERE */}
+      <div className="aside_link_flex">
+        <p className={Asideheading}>Overview</p>
+        <Link to="/admin" className="aside_links">
+          {" "}
+          <TfiStatsUp className={aside_icon_state} />{" "}
+          <span className={Asidespan}>Overview</span>{" "}
+        </Link>
+      </div>
+      {/* ORDER */}
+      <div className="aside_link_flex">
+        <p className={Asideheading}>Orders</p>
+        <Link className="aside_links" to="/user/order">
+          {" "}
+          <MdOutlinePendingActions className={aside_icon_state} />
+          <span className={Asidespan}> My Orders</span>
+        </Link>
+      </div>
+      {/* WISHLISTS */}
+      {/* SETTINGS */}
+      {/* vender OPTION REQUEST */}
+      {/* Contact US */}
+      <div className="aside_link_flex">
+        <p className={Asideheading}>Actions</p>
+        <Link to="/logout" className="aside_links">
+          {" "}
+          <BiLogOutCircle className={aside_icon_state} />{" "}
+          <span className={Asidespan}>Logout</span>{" "}
+        </Link>
+      </div>
+    </aside>
+  );
+};
 
-
-        </aside>
-    )
-}
-
-export default AdminSidebar
+export default AdminSidebar;
