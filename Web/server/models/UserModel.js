2213994bbe46b6e01fccae23e68316ceab5d77a6
hassan6846 <96440779+hassan6846@users.noreply.github.com>
2023-10-05 22:58:59 +0500

    Update UserModel.js

--- Web/server/models/UserModel.js
+++ Web/server/models/UserModel.js
@@ -32,7 +32,7 @@ avatar: {
     url: {
       type: String,
       required: false,
-     default:"https://randomuser.me/api/"
+     default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     }
   },
   role: {
