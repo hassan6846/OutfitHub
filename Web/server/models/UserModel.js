5b7454081e99c07fe7ff29df307c2036201fab68
hassan6846 <96440779+hassan6846@users.noreply.github.com>
2023-10-06 11:45:50 +0500

    main

--- Web/server/models/UserModel.js
+++ Web/server/models/UserModel.js
@@ -1,30 +1,29 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bycrypt = require("bcrypt");
+const crypto = require("crypto");
+const validator = require("validator");
 
-const mongoose=require("mongoose");
-const jwt =require("jsonwebtoken");
-const bycrypt=require("bcrypt")
-const crypto =require("crypto")
-const validator=require("validator")
-
-const UserSchema=new mongoose.Schema({
-username:{
-    type:String,
-    require:[true,"Please  Enter  your name"],
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: [true, "Please  Enter  your name"],
     maxLength: [30, "Name cannot exceed 30 characters"],
     minLength: [4, "Name should have more than 4 characters"],
-},
-email:{
+  },
+  email: {
     type: String,
     required: [true, "Please Enter Your Email"],
     unique: true,
     validate: [validator.isEmail, "Please Enter a valid Email"],
-},
-password:{
+  },
+  password: {
     type: String,
     required: [true, "Please Enter Your Password"],
     minLength: [8, "Password should be greater than 8 characters"],
     select: false,
-},
-avatar: {
+  },
+  avatar: {
     public_id: {
       type: String,
       required: false,
@@ -32,16 +31,17 @@ avatar: {
     url: {
       type: String,
       required: false,
-     default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-    }
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
   },
   role: {
     type: String,
     default: "user",
   },
-  phone:{
-    type:Number,
-   default:""
+  phone: {
+    type: Number,
+    default: "",
   },
   createdAt: {
     type: Date,
@@ -54,15 +54,13 @@ avatar: {
 
   resetPasswordToken: String,
   resetPasswordExpire: Date,
-  lastPasswordUpdate:Date
+  lastPasswordUpdate: Date,
+});
 
-})
-
-
-//HASHING PASSWORD BEFORE SAFE METHOD.. 
-UserSchema.pre('save', async function (next) {
+//HASHING PASSWORD BEFORE SAFE METHOD..
+UserSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
-    this.password = await bycrypt.hash(this.password, 10); 
+    this.password = await bycrypt.hash(this.password, 10);
   }
   next();
 });
@@ -72,7 +70,7 @@ UserSchema.pre('save', async function (next) {
 UserSchema.methods.comparePassword = async function (password) {
   return await bycrypt.compare(password, this.password);
 };
-const User=mongoose.model("User",UserSchema)
+const User = mongoose.model("User", UserSchema);
 // Reset password TOKEN
 
-module.exports=User
+module.exports = User;
