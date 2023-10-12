f64f3674b2345312b4303095e98072788a6fc5df
hassan6846 <96440779+hassan6846@users.noreply.github.com>
2023-10-12 21:23:48 +0500

    main

--- Web/client/src/pages/admin/Components/AddProduct.js
+++ Web/client/src/pages/admin/Components/AddProduct.js
@@ -1,16 +1,40 @@
 import React from 'react'
 import "./AddProduct.css"
-
+import {MDBInput} from "mdb-react-ui-kit"
 
 const AddProduct = () => {
   return (
     <div className='addproduct-wrapper'>
-    
-<div className='addproduct_container'>  
-<p  style={{display:"flex",alignItems:"center",justifyContent:"space-between"}} className='wishlist_page_head'>Add Product <span></span> </p>
+
+      <div className='addproduct_container'>
+        <p style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }} className='wishlist_page_head'>Add Product <span></span> </p></div>
 
 
-</div>
+      {/* ADD OTHER COMPONENTS BELLOW  is Card of Box shadow*/}
+      <div className='add_product_card_main'>
+        <div className='add_product_1_flex'>
+          <p className='product_setting_head'>Product Settings</p>
+          <p style={{ color: '#848484', fontSize: "0.8rem", fontWeight: "500" }}>Product Images</p>
+          {/* Image Preview GRid */}
+          <div className='images_upload_grid'>
+
+          </div>
+
+        </div>
+        {/* Onlyforms */}
+        <div className='add_product_2_flex'>
+        <div>
+          <h3 className="user_name">Full Name </h3>
+          <p className="user_name_ans">
+            <MDBInput size="lg" placeholder="Full Name" />
+          </p>
+        </div>
+        {/* 2 */}
+        
+        </div>
+
+      </div>
+      {/*  */}
     </div>
   )
 }
--- Web/client/src/pages/admin/Components/Overview.js
+++ Web/client/src/pages/admin/Components/Overview.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import GetTime from '../../../helpers/GetCurrentDate'
+import React from "react";
+
 const Overview = () => {
   return (
     <div>
-      <p style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }} className='wishlist_page_head'>Hi Admin, <span><p style={{ backgroundColor: "#F9F9F9", fontSize: "18px", padding: "0.4rem", border: "1px solid #ddc3c3", borderRadius: "5px", marginBottom: "0px", userSelect: "none" }}>{GetTime()}</p></span> </p>
-    </div>
-  )
-}
+      <p
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+        className="wishlist_page_head"
+      >
+        Hi Admin,{" "}
+        <span>
 
-export default Overview
+        </span>{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Overview;
