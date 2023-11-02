886250eddfb1b47e6f17bde3a5977d63faf7841d
hassan6846 <96440779+hassan6846@users.noreply.github.com>
2023-11-02 19:30:38 +0500

    main

--- Web/server/Controllers/ProductController.js
+++ Web/server/Controllers/ProductController.js
@@ -1,15 +1,5 @@
-// all controllers
-/**
- * create and upload a new product --admin
- * --get all products
- * --update products state after purchase 
- * --delete entiere product
- * getproductDetails
- * --create Product Review by User
- * --Delete Users Review
- * GetAdmin products
- */
 const Product = require("../models/ProductSchema");
+const { cloudinary } = require("../utils/Cloudinary");
 
 // Controller function to create a new product
 const createProduct = async (req, res) => {
