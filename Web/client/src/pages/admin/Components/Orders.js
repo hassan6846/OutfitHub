2a794536691066414c10400ccf97f86aa04a4b9c
hassan6846 <96440779+hassan6846@users.noreply.github.com>
2023-11-07 19:40:21 +0500

    main

--- Web/client/src/pages/admin/Components/Orders.js
+++ Web/client/src/pages/admin/Components/Orders.js
@@ -18,10 +18,12 @@ const Orders = () => {
 }
 const dropdown_collapsableClass = collapsable ? "dropdown_view_toggle" : "dropdown_view"
   return (
+    <>
+    <p style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }} className='wishlist_page_head'>Orders <span></span> </p>
+
     <div className='dropdown_wrapper_datagrid'>
   
-      <p style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }} className='wishlist_page_head'>Orders <span></span> </p>
-
+  
      <section>
       <div className='orderDatagrid'>
 
@@ -44,7 +46,7 @@ const dropdown_collapsableClass = collapsable ? "dropdown_view_toggle" : "dropdo
         <div className='action_Wrapper'>
           <Link><FaEdit size={20} /></Link>
           <Link><FaEye size={20} /></Link>
-          <Link onClick={collapsableToggle}><BsFillCaretDownFill size={20} /></Link>
+          <Link className='dropdown_Link_collapsble' onClick={collapsableToggle}><BsFillCaretDownFill size={20} /></Link>
          
         </div>
       </div>
@@ -63,8 +65,54 @@ const dropdown_collapsableClass = collapsable ? "dropdown_view_toggle" : "dropdo
     </div>
 
     </section>
+
+  
+    <section>
+      <div className='orderDatagrid'>
+
+        {/* product Image */}
+        <div className='Image_description'>
+
+          <Checkbox className='datagridCheck' style={{ marginRight: "0.5rem" }} />
+          <LazyLoadImage            wrapperClassName="imageMain_product_order"  effect="opacity" className='product_imageMain' src={ImageAddress} alt='img'  />
+          <p  className="datagrid_title"style={{ marginBottom: "0" }}>Fuji Film 2k Camera</p>
+          <p className="mobile_sku" style={{ marginBottom: "0" }}>Tr-001</p>
+        </div>
+        <p className='desktop_sku' style={{ marginBottom: "0" }}>SKU-001</p>
+        <p className='stock_status' style={{ marginBottom: "0" }}>InStock <span style={{ color: "red" }}>(2)</span></p>
+        <p className='datagrid_category' style={{ marginBottom: "0" ,color:"white",fontWeight:"500",backgroundColor:"#4BB497",padding:"0.2rem",borderRadius:"2px"}}>Electronics</p>
+        <p className='datagrid_sub' style={{ marginBottom: "0" }}>Camera</p>
+      
+        <p className='price_datagrid' style={{ marginBottom: "0" }}>254$ <span style={{ textDecoration: "line-through", fontSize: "13px" }}>300$</span></p>
+        <p className='Last_updated' style={{ marginBottom: "0" }}>Last Modified: <br /> 28/4/2023</p>
+        {/* Actions */}
+        <div className='action_Wrapper'>
+          <Link><FaEdit size={20} /></Link>
+          <Link><FaEye size={20} /></Link>
+          <Link className='dropdown_Link_collapsble' onClick={collapsableToggle}><BsFillCaretDownFill size={20} /></Link>
+         
+        </div>
+      </div>
+
+{/* collapsble */}
+    <div className={dropdown_collapsableClass}>
+      <div className='dropdown_collapsable'> 
+        <div className='product_table_head'style={{fontSize:"1.2rem"}}>Product</div>
+        <div className='image_table'><img alt="product_img" className='product_table_image' src={ImageAddress}/> <p style={{margin:"0"}}>Xiaomi NoteBook </p></div>
+        <div className='product_table_head'>Sku:<span style={{fontSize:'13px',fontWeight:"500"}}>TR-001</span></div>
+        <div className='product_table_head'>Stock status:<span style={{color:"#4BB497",fontWeight:"bold"}}>In Stock</span> (120)</div>
+        <div className='product_table_head'>Price: <span style={{fontSize:'13px',fontWeight:"500"}}>$254</span></div>
+        <div className='product_table_head'>Category:<span style={{color:"rgb(0,107,255)",fontWeight:"bold"}}>Electronics</span></div>
+      </div>
+      {/* ends here */}
     </div>
 
+    </section>
+
+
+
+    </div>
+    </>
   )
 }
 
