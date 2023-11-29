ae19d5c35cfd993068316b41172ec69fe6d92447
Hassan <96440779+hassan6846@users.noreply.github.com>
2023-11-29 15:54:44 +0500

    Revert "adding authkit"
    This reverts commit 4970f211cf9058e660ccb7fb5753dbd80b333578.

--- Web/client/src/index.js
+++ Web/client/src/index.js
@@ -10,16 +10,10 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 // components or hooks
 import { Provider } from 'react-redux';
 import store from './store/Store'
-// kit
-import {AuthProvider } from "react-auth-kit"
 const root = ReactDOM.createRoot(document.getElementById('$2y$10$/AxPzoEC96Ldhel1jdTvrO4q6ZU2GBFmLvaG3/00ynZKWnUqVmgWS'));
 root.render(
     <Provider store={store}>
-    <AuthProvider 
-    
-    >
     <App />
-    </AuthProvider>
     </Provider>
 );
 reportWebVitals();
