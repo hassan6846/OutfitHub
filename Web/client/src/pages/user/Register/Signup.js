8b36bf531431445036999ad8a18ec24844f765de
Hassan <96440779+hassan6846@users.noreply.github.com>
2023-12-03 18:12:43 +0500

    main

--- Web/client/src/pages/user/Register/Signup.js
+++ Web/client/src/pages/user/Register/Signup.js
@@ -21,24 +21,20 @@ const Signup = () => {
                 <div className="signup_form_wrapper">
                     <h1 className="signup_head">Sign up for a account</h1>
                     <p className="para_signup" >No credit card required. Already have an account?<Link to="/login" className="Link_login">Log in.</Link></p>
-
                     <InputProps title="E-mail" type="Email" />
-
                     <div className="input_flex_signup">
                         <InputProps title="First name" type="text" />
                         <InputProps title="Last name" type="text" />
                     </div>
                     <InputProps title="Password" type="password" />
                     <InputProps title="Confirm password" type="password" />
-                {/* Country & Number */}
-                
+                    {/* Country & Number */}
                     <InputProps title="Phone Number" type="number" />
-          
-          <div className="btn_next"> 
-          <MDBBtn  style={{backgroundColor:"#4BB497"}} className="next_action">Create my account</MDBBtn>
-          </div>
-               
-               
+                    <div className="btn_next">
+                        <MDBBtn style={{ backgroundColor: "#4BB497" }} className="next_action">Create my account</MDBBtn>
+                    </div>
+
+
                 </div>
             </section>
             <Footer />
