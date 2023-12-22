0d19a955d33fe307b8c21d3f37635f2524c5fcd1
Hassan <96440779+hassan6846@users.noreply.github.com>
2023-12-22 17:55:44 +0500

    Update Products.js

--- Web/client/src/pages/user/Shop/Products.js
+++ Web/client/src/pages/user/Shop/Products.js
@@ -1,11 +1,11 @@
 import React from 'react';
 // components
 import CategoryBar from '../../../components/CategoryBar/CategoryBar';
-import  Alert from "../../../components/Alert/Alert"
-// css
-import './products.css';
 import ProductContainer from '../../../components/Infinite-Container_products/ProductContainer';
 import Footer from "../../../Layouts/footer/Footer"
+// css
+import './products.css';
+
 
 const Products = () => {
 
