8db86a44be312d8e9b0057d3beee19ca57f8278f
Hassan <96440779+hassan6846@users.noreply.github.com>
2023-12-24 21:10:11 +0500

    Cookie-Consent Fix

--- Web/client/src/App.js
+++ Web/client/src/App.js
@@ -3,10 +3,6 @@ import React from "react";
 import AllRoutes from "./Routing";
 // Auth
 import { AuthProvider } from "react-auth-kit"
-import { Toaster } from "react-hot-toast"
-//cookie consent
-import { CookieConsent } from "react-cookie-consent"
-//css
 import "./app.css"
 
 /**
@@ -24,22 +20,8 @@ function App() {
         cookieDomain={window.location.hostname}
         cookieSecure={false}
       >
-        <Toaster />
+     
         <AllRoutes />
-        <CookieConsent
-          acceptOnScroll={true}
-          acceptOnScrollPercentage={5}
-          location="bottom"
-          buttonText="Accept."
-          cookieName="consent"
-          buttonStyle={{ color: "#4e503b", fontSize: "13px", padding: "0.2rem 1rem", right: "0" }}
-          expires={150}
-          style={{ right: '5px' }}
-        >
-          This website uses cookies to ensure you get best experience at out webiste{" "}
-          <span style={{ fontSize: "10px", textDecoration: "underline", marginLeft: "0.3rem", color: "#4BB497", fontWeight: "bold", cursor: "pointer" }}>learn more about privacy policiy.</span>
-
-        </CookieConsent>
       </AuthProvider>
     </QueryClientProvider>
   );
