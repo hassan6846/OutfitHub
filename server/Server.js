ee3263ccbbe23c617dcc51c4711d780addac04d4
Hassan <96440779+hassan6846@users.noreply.github.com>
2023-12-27 01:34:45 +0500

    Main

--- server/Server.js
+++ server/Server.js
@@ -1,32 +1,7 @@
-const app = require("./app");
-const dbConnect = require("./config/dbConnect");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-//handling errros
-process.on("uncaughtException", (err) => {
-  console.log(`Error: ${err.message}`);
-  console.log(`Shutting down the server due to Uncaught Exception`);
-  process.exit(1);
-});
-//config
-//   if (process.env.NODE_ENV !== "PRODUCTION") {
-//     require("dotenv").config({ path: "backend/config/config.env" });
-//   }
-// Connect to the database
-dbConnect();
+const Cat = mongoose.model('Cat', { name: String });
 
-//listening
-const port = process.env.PORT;
-app.listen(port, function (req, res) {
-  console.log(process.env.MSG);
-  console.log(`app is running on ${port}`);
-});
-
-// Unhandled Promise Rejection
-process.on("unhandledRejection", (err) => {
-  console.log(`Error: ${err.message}`);
-  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
-
-  server.close(() => {
-    process.exit(1);
-  });
-});
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
--- server/app.js
+++ server/app.js
@@ -7,22 +7,11 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
-const serviceAccount = require('./firebase/design-ecommerce-dd3eb-firebase-adminsdk-jekf4-88a2021ea7.json')
-const admin = require("firebase-admin")
-//firebase Initialize
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://design-ecommerce-dd3eb-default-rtdb.firebaseio.com/"
-})
-// Importing all routes
-const user = require("./Routes/UserRoutes")
-const product = require("./Routes/ProductRoutes");
-const { sendEmail } = require("./utils/SendMail");
-// const payment= require("./Routes/PaymentRoute")
-//admin routes
 
 
-// Middlewares
+
+
+// MiddleWare
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -33,21 +22,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.json({
   limit: '100mb'
 }));
-
-
 app.use(fileUpload())
 app.disable('x-powered-by')
 /////
 
 
-app.get('/send', sendEmail);
-//routes
-app.use("/api/v1", user)
-// app.use("/api/v1",payment)
-app.use("/api/v1", product)
-// app.use("/api/v1",orders)
-// app.use("/api/v1", payment);
-
-// Listen for upload Images.
 
 module.exports = app
