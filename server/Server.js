ee3ce171ef56ce90ea229fb2ac5bacc537c67d7d
Hassan <96440779+hassan6846@users.noreply.github.com>
2023-12-30 13:59:33 +0500

    ConnectDb

--- server/Server.js
+++ server/Server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const { connectDb } = require("./config/dbConnect");
-
+connectDb()
 app.listen(process.env.PORT,()=>{
     console.log(`Server is Running on ${process.env.PORT}`)
 })
--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,8 +1,8 @@
 const  mongoose  = require("mongoose");
 //main DBConnnect Function..
-function connectDb(){
+async function connectDb(){
     try {
-        mongoose.connect('mongodb://127.0.0.1:27017/Ecommerce', {
+    await mongoose.connect('mongodb://127.0.0.1:27017/Ecommerce', {
           useNewUrlParser: true,
           useUnifiedTopology: true,
         })
