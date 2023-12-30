4f454573e6ee1feca8429bff3974a82cb4eb4689
Hassan <96440779+hassan6846@users.noreply.github.com>
2023-12-30 14:08:39 +0500

    connectdb

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,19 +1,19 @@
-const  mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 //main DBConnnect Function..
-async function connectDb(){
-    try {
+async function connectDb() {
+  try {
     await mongoose.connect('mongodb://127.0.0.1:27017/Ecommerce', {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        })
-          .then(() => {
-            console.log('DB connected');
-          })
-          .catch((err) => {
-            console.error('DB connection error:', err.message);
-          });
-      } catch (err) {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+      .then(() => {
+        console.log('DB connected');
+      })
+      .catch((err) => {
         console.error('DB connection error:', err.message);
-      }
+      });
+  } catch (err) {
+    console.error('DB connection error:', err.message);
+  }
 }
-module.exports={connectDb}
+module.exports = { connectDb }
