f515ed786971393d1aa18a4ad0606007f9d3cff3
Hassan <96440779+hassan6846@users.noreply.github.com>
2023-12-31 05:47:44 +0500

    Update UserControllers.js

--- server/Controllers/UserControllers.js
+++ server/Controllers/UserControllers.js
@@ -1,7 +1,11 @@
 const UserModel = require("../models/UserModel");
+// User Controller.
 const { registerUser } = require("./UserController/Register");
 const { loginUser } = require("./UserController/Login");
 const { Userlogout } = require("./UserController/Logout");
 const { ForgotPassword } = require("./UserController/ForgotPassword");
+//Admin Controller
+const { getAllUser } = require("./UserController/AllUsers.admin");
 
 
+module.exports = { registerUser, loginUser, Userlogout, ForgotPassword, getAllUser };
