b1cd9b4d26f09d92a44a09a27f769f37198a890b
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-01-12 14:52:12 +0500

    added 404 page

--- build/src/app/not-found.js
+++ build/src/app/not-found.js
@@ -1,6 +1,22 @@
 import Link from "next/link"
-
-
+//styles
+import styles from "./not-found.module.css"
 export default function NotFound() {
-    return <h1>404 - Page Not Found</h1>
+    return <>
+        <div>
+            <div id="notfound">
+                <div class="notfound">
+                    <div class="notfound-404">
+                        <h3>Oops! Page not found</h3>
+                        <h1><span>4</span><span>0</span><span>4</span></h1>
+                    </div>
+                    <h2>we are sorry, but the page you requested was not found</h2>
+                    {/* <BiLeftArrowAlt /> */}
+                    <Link href="/" className='HomeLink'> Go BackHome</Link>
+                </div>
+            </div>
+
+        </div>
+
+    </>
 }
