15b713a83de0dd5cddcfbe0c65abd9e13bb08a46
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-01-12 15:06:09 +0500

    main

--- build/src/app/not-found.js
+++ build/src/app/not-found.js
@@ -4,15 +4,15 @@ import styles from "./not-found.module.css"
 export default function NotFound() {
     return <>
         <div>
-            <div id="notfound">
-                <div class="notfound">
-                    <div class="notfound-404">
+            <div className={styles.NotFoundCont}>
+                <div className={styles.notfound}>
+                    <div className={styles.notfoundError}>
                         <h3>Oops! Page not found</h3>
                         <h1><span>4</span><span>0</span><span>4</span></h1>
                     </div>
                     <h2>we are sorry, but the page you requested was not found</h2>
                     {/* <BiLeftArrowAlt /> */}
-                    <Link href="/" className='HomeLink'> Go BackHome</Link>
+                    <Link href="/" className={styles.HomeLink}> Go BackHome</Link>
                 </div>
             </div>
 
