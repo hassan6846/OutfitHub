ee04fcbb3787c4a6f7a3e49b2c2b48428f3f0bd3
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-01-15 21:14:47 +0500

    main

--- client_vite/src/main.jsx
+++ client_vite/src/main.jsx
@@ -1,11 +1,12 @@
+//React Render.
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+//Components
 import App from './App.jsx'
 import './index.css'
 //React Query
-import {} from "@tanstack/react-query"
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+  <App />
 
 )
