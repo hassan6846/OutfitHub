440d5bd2bb4613be9942090b56b5070a4dc356e5
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-01-26 21:35:51 +0500

    main

--- client_vite/src/components/SliderHome/SliderHome.jsx
+++
@@ -1,8 +0,0 @@
-//Swiper
-import {Swiper,SwiperSlide} from "swiper/react"
-import "swiper/css"
-export const SliderHome = () => {
-    return (
-        <div>SliderHome</div>
-    )
-}
--- client_vite/src/store/Store.jsx
+++
@@ -1,5 +0,0 @@
-import {legacy_createStore} from  "@reduxjs/toolkit"
-//All Slices
-const store=legacy_createStore({
-    
-})
