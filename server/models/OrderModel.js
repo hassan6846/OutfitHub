673773b9b59288dfd8697b07e96d49a4f043cc2b
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-11-07 20:13:12 +0500

    main

--- server/models/OrderModel.js
+++ server/models/OrderModel.js
@@ -1,5 +1,30 @@
 const mongoose=require('mongoose')
 
 const OrderSchema=new mongoose.Schema({
+    OrderedAt:{
+        default:Date.now,
+        required:false,
+    },
+    Total:{
 
+    },
+    Status:{
+
+    },
+    PaymentType:{
+
+    },
+    PaidOnline:{
+
+    },
+ products:{
+    type:[Object]
+ },
+ orderedBy:{
+    type:mongoose.Types.ObjectId
+
+ },
+ OrderDetails:{
+    //ref the address Schema if you want.. address book
+ }
 })
