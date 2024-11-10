61dab2ad842533b7e4140f407388da2c8934a279
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-11-10 19:45:12 +0000

    main

--- server/Controllers/Product/GetSingleProduct.js
+++ server/Controllers/Product/GetSingleProduct.js
@@ -3,5 +3,25 @@ const Product = require("../../models/ProductSchema");
 
 
 const GetSingleProduct=async(req,res,next)=>{
-    
+    const {id}=req.params
+  if(!id){
+    return res.status(400).json({
+        success: false,
+        msg: "Please fill all required fields",
+    });
+  }
+  try {
+    const product=await Product.findById(id)
+    res.json({
+        sucess:true,
+        data:product
+    })
+  } catch (error) {
+    console.error("Error creating product:", error);
+    return res.status(500).json({
+        success: false,
+        message: "Internal server error please check your internet",
+    });
+  }
 }
+module.exports=GetSingleProduct
