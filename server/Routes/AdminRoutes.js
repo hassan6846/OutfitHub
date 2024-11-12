8bd11232edb80a8b23f419ce9d153d2f58c10829
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-11-12 17:02:42 +0500

    Update AdminRoutes.js

--- server/Routes/AdminRoutes.js
+++ server/Routes/AdminRoutes.js
@@ -8,11 +8,13 @@ const CreateProduct = require('../Controllers/Product/CreateProduct')
 const AllProducts = require('../Controllers/Product/GetAll')
 const DeleteProduct = require('../Controllers/Product/DeleteProduct')
 
+
 //middlewares
 
-
+const { isAuthenticated,isAuthorized } = require('../middlewares/Auth')
 //routes
-router.route('/admin/user-count').get(CountUser) //sends the number of registed users on the app.
+router.route('/admin/user-count').get(isAuthenticated, isAuthorized("admin"), CountUser);
+
 router.route('/admin/get-users').get(GetUsers)
 
 router.route('/admin/product/upload').post(CreateProduct)
