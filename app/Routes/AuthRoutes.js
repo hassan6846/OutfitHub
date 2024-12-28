baa9c3d91639061dcd45780bc48d15e0730a3f26
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-12-28 14:13:53 +0500

    refactor: format AuthRoutes.js for improved readability

--- app/Routes/AuthRoutes.js
+++ app/Routes/AuthRoutes.js
@@ -1,15 +1,15 @@
-const express=require("express")
-const router=express.Router()
+const express = require("express")
+const router = express.Router()
 //controllers
-const  Register  = require("../Controllers/Auth/Register");
-const Login=require("../Controllers/Auth/Login");
+const Register = require("../Controllers/Auth/Register");
+const Login = require("../Controllers/Auth/Login");
 const Logout = require("../Controllers/Auth/Logout");
 
 //middlewares
 const LoginRequestLimits = require("../utils/RateLimit");
 const { isAuthenticated } = require("../middlewares/Auth");
 //Routes
-router.route('/register').post(LoginRequestLimits,Register)
-router.route('/login').post(LoginRequestLimits,Login)
-router.route('/logout').post(isAuthenticated,Logout)
-module.exports=router
+router.route('/register').post(LoginRequestLimits, Register)
+router.route('/login').post(LoginRequestLimits, Login)
+router.route('/logout').post(isAuthenticated, Logout)
+module.exports = router
