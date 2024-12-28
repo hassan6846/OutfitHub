067122233efadb60d172820db18458d04ebd0bbb
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-12-28 13:58:39 +0500

    feat: add Address model import and create GetAddress controller

--- app/models/AddressBook.js
+++ app/models/AddressBook.js
@@ -1,28 +1,31 @@
 const { default: mongoose } = require("mongoose");
 
 
+const AddressSchema = new mongoose.Schema({
+    addressTitle: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        required: true,
+        ref: 'User'
 
-const AddressSchema=new mongoose.Schema({
-    addressTitle:{
-        type:String,
-        required:true,
-        trim:true,
     },
-    city:{
-        type:String,
-        required:true
+    phone: {
+        type: String,
+        required: true,
     },
-    user:{
-         type:mongoose.Schema.ObjectId,
-         required:true,
-    },
-    phone:{
-        type:String,
-        required:true,
-    }
 
-},{
-    timestamps:true
-})
-const Address= mongoose.model('Address',AddressSchema)
-module.exports=Address
+}, {
+    timestamps: true
+}
+
+)
+const Address = mongoose.model('Address', AddressSchema)
+module.exports = Address
