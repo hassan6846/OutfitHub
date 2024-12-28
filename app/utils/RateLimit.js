eff4688ff4d756fc7670475586f1efa6188d30a2
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-12-28 14:07:47 +0500

    refactor: adjust rate limit settings and clean up logout route

--- app/utils/RateLimit.js
+++ app/utils/RateLimit.js
@@ -1,9 +1,10 @@
 const RateLimit = require("express-rate-limit")
 const LoginRequestLimits = RateLimit({
     windowMs: 60 * 60 * 1000, // 1hrs
-    limit: 500,
+    limit: 10,
     standardHeaders: 'draft-7', // Set `RateLimit` and `RateLimit-Policy` headers
     legacyHeaders: false,
     message: { message: 'Too many Requests from this IP, please try again after an hour, Kindly Dont Spam Server' }
+
 })
-module.exports=LoginRequestLimits
+module.exports = LoginRequestLimits
