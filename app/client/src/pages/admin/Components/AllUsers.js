5fc8ab19f1717677f7880c05b002bb3ad9daf6dd
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-12-29 16:02:44 +0500

    fix: add loading spinner to AllUsers component; improve user experience during data fetching

--- app/client/src/pages/admin/Components/AllUsers.js
+++ app/client/src/pages/admin/Components/AllUsers.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 //imports
 import { ENDPOINT } from "../../../api/Endpoint"
-
+import CircularProgress from '@mui/material/CircularProgress';
 
 //styles
 import "./AllUsers.css"
@@ -32,6 +32,7 @@ const AllUsers = () => {
   //useEffect
   useEffect(() => {
     const fetchData = async () => {
+      isloading(true)
       try {
         const [userCountResponse, usersResponse, uploadResponse, productResponse, storageusageResponse] = await Promise.all([
           axios.get(`${ENDPOINT}/admin/user-count`, { withCredentials: true }),
@@ -56,82 +57,69 @@ const AllUsers = () => {
   }, [])
 
   return (
-
-
-
     <div>
-      <p style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }} className='wishlist_page_head'>User Manegment,   </p>
-      <p style={{ fontSize: "1.1rem", marginBottom: "0.4rem", color: "#131039", padding: "0.4rem", fontWeight: "500" }}>Users Overview,/stats</p>
-
-
-
-      <div className='row_user_data_cards'>
-        <div className='user_manegment_card'> <p className='usersCount'>  <span className='icons_flex_user_maneg'><PiUsersThree size={70} className='icon_user_maneg' /></span> {usercount}</p> <p>Total Registerd Users.</p>
-
-
+      {loading ? (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <CircularProgress />
         </div>
-
-
-
-        <div className='user_manegment_card'> <p className='usersCount'>  <span className='icons_flex_user_maneg'><SlGraph size={70} className='icon_user_maneg' /></span> {uploads}</p> <p>Total Image Asset Hosted.</p></div>
-        <div className='user_manegment_card'> <p className='usersCount'>  <span className='icons_flex_user_maneg'><FaHandsHelping size={70} className='icon_user_maneg' /></span> {productcount}</p> <p>Total Product Uploaded</p></div>
-        <div className='user_manegment_card'> <p className='usersCount'>  <span className='icons_flex_user_maneg'><RiRadioButtonLine size={70} className='icon_user_maneg' /></span> {Math.floor(storageusage)} mb</p> <p>Total Cloud Storage used</p></div>
-      </div>
-
-      {/* Graphs Vistors World And New Users */}
-      <div className=''> </div>
-      <p style={{ fontSize: "1.1rem", marginBottom: "0.4rem", color: "#131039", padding: "0.4rem", fontWeight: "500" }}>All Users.</p>
-
-      {/* div For all users... */}
-      <div className='all_users_datagrid'>
-        {
-          allusers.map((user) => (
-            <div className="user-Card">
-              <div style={{ flexDirection: "row", display: "flex", alignItems: "center", justifyContent: "space-between" }} >
-                <div>
-
-
-                  <Avatar src={user.avatar} />
-                  <p>{user.username}</p>
-                </div>
-
-                <div>
-                  <p style={{ marginBottom: "3px", fontWeight: "bold" }}>Details</p>
-                  <p style={{ marginBottom: "3px" }}>Email:{user.email}</p>
-                  <p style={{ marginBottom: "3px" }}>Phone:{user.phone}</p>
-                  <p></p>
-                </div>
-                <div>
-                  <p style={{ marginBottom: "3px", fontWeight: "bold" }}>Joined On</p>
-                  <p>{user.createdAt}</p>
-                </div>
-
-                <div>
-                  <p style={{ marginBottom: "3px", fontWeight: "bold" }}>Role</p>
-                  <p>{user.role}</p>
-                </div>
-
-                <div>
-                  <p style={{ marginBottom: "3px", fontWeight: "bold" }}>Actions</p>
-                  <MoreVertIcon onClick={handleMenuOpen} style={{ cursor: "pointer" }} />
-                  <Menu
-                    anchorEl={anchorEl}
-                    open={Boolean(anchorEl)}
-                    onClose={handleMenuClose}
-                  >
-
-                    <MenuItem onClick={handleMenuClose}>Delete User</MenuItem>
-                  </Menu>
-                </div>
-              </div>
-
+      ) : (
+        <>
+          <p style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }} className='wishlist_page_head'>User Manegment,   </p>
+          <p style={{ fontSize: "1.1rem", marginBottom: "0.4rem", color: "#131039", padding: "0.4rem", fontWeight: "500" }}>Users Overview,/stats</p>
+          <div className='row_user_data_cards'>
+            <div className='user_manegment_card'> <p className='usersCount'>  <span className='icons_flex_user_maneg'><PiUsersThree size={70} className='icon_user_maneg' /></span> {usercount}</p> <p>Total Registerd Users.</p>
             </div>
-          ))
-        }
-      </div>
+            <div className='user_manegment_card'> <p className='usersCount'>  <span className='icons_flex_user_maneg'><SlGraph size={70} className='icon_user_maneg' /></span> {uploads}</p> <p>Total Image Asset Hosted.</p></div>
+            <div className='user_manegment_card'> <p className='usersCount'>  <span className='icons_flex_user_maneg'><FaHandsHelping size={70} className='icon_user_maneg' /></span> {productcount}</p> <p>Total Product Uploaded</p></div>
+            <div className='user_manegment_card'> <p className='usersCount'>  <span className='icons_flex_user_maneg'><RiRadioButtonLine size={70} className='icon_user_maneg' /></span> {Math.floor(storageusage)} mb</p> <p>Total Cloud Storage used</p></div>
+          </div>
+          {/* Graphs Vistors World And New Users */}
+          <div className=''> </div>
+          <p style={{ fontSize: "1.1rem", marginBottom: "0.4rem", color: "#131039", padding: "0.4rem", fontWeight: "500" }}>All Users.</p>
+          {/* div For all users... */}
+          <div className='all_users_datagrid'>
+            {
+              allusers.map((user) => (
+                <div className="user-Card">
+                  <div style={{ flexDirection: "row", display: "flex", alignItems: "center", justifyContent: "space-between" }} >
+                    <div>
+                      <Avatar src={user.avatar} />
+                      <p>{user.username}</p>
+                    </div>
+                    <div>
+                      <p style={{ marginBottom: "3px", fontWeight: "bold" }}>Details</p>
+                      <p style={{ marginBottom: "3px" }}>Email:{user.email}</p>
+                      <p style={{ marginBottom: "3px" }}>Phone:{user.phone}</p>
+                      <p></p>
+                    </div>
+                    <div>
+                      <p style={{ marginBottom: "3px", fontWeight: "bold" }}>Joined On</p>
+                      <p>{user.createdAt}</p>
+                    </div>
+                    <div>
+                      <p style={{ marginBottom: "3px", fontWeight: "bold" }}>Role</p>
+                      <p>{user.role}</p>
+                    </div>
+                    <div>
+                      <p style={{ marginBottom: "3px", fontWeight: "bold" }}>Actions</p>
+                      <MoreVertIcon onClick={handleMenuOpen} style={{ cursor: "pointer" }} />
+                      <Menu
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={handleMenuClose}
+                      >
+                        <MenuItem onClick={handleMenuClose}>Delete User</MenuItem>
+                      </Menu>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </>
+      )}
     </div>
   )
-
 }
 
 export default AllUsers
