8641d5ed61ee2fdbe82d55e502b94068ac01e7a2
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-12-29 19:31:26 +0500

    fix: add chat UI component to Contact page; integrate @chatscope/chat-ui-kit-react for enhanced user interaction

--- app/client/src/pages/user/UserProfile/Contact.js
+++ app/client/src/pages/user/UserProfile/Contact.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import { MDBBtn, MDBInput, MDBTextArea } from "mdb-react-ui-kit"
 import "./Contact.css"
-const Contact = () => {
-  return (
-    <div className='container_CONTACT'>
-      <p className='contact_us'>Contact Us.</p>
-      <p className='contact_us_sub'>Feel Free to Contact Us.</p>
-      <div className='contact_inputs_form'>
-        <MDBInput label='Name' type='text' size='lg' />
-        <MDBInput label='Email' type='text' size='lg' />
-        <MDBInput label='Subject' placeholder='Title/Subject' type='text' size='lg' />
-        <MDBTextArea placeholder='Please Dont Enter Links Here Just type The Reason or Problem You are facing' label='Message' id='textAreaExample' rows={10} />
-        <MDBBtn style={{ backgroundColor: "#4BB497", color: "white" }}>Submit</MDBBtn>
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput } from "@chatscope/chat-ui-kit-react";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+
+
+  const Contact = () => {
+    return (
+      <div className='container_CONTACT'>
+        
+   
+        <div style={{ height: "110vh" ,width:"100%"}}>
+          <MainContainer>
+            <ChatContainer>
+              <MessageList>
+                <Message model={{ message: "So please be patient, the Martian will surely answer you or say we can reply to you as soon as possible", sentTime: "just now", sender: "system" }} />
+              </MessageList>
+              <MessageInput placeholder="Type your message here..." />
+            </ChatContainer>
+          </MainContainer>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+
+
 
 export default Contact
