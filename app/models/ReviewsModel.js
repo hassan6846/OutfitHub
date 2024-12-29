09b14b2722b719b22c0010a9a6c9458447447617
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-12-29 12:27:45 +0500

    feat: implement ReviewsModel.js for managing product reviews

---
+++ app/models/ReviewsModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new mongoose.Schema({
+    product: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+ 
+module.exports = mongoose.model('Review', ReviewSchema);
