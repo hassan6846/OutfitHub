f7a627aa3238e25dd0353959a01a8d47f744623a
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-12-29 12:36:30 +0500

    refactor: simplify CORS configuration in app.js and fix request parameter in route handler

--- app/app.js
+++ app/app.js
@@ -17,11 +17,7 @@ require("dotenv").config();
 app.use(bodyParser.json({type: 'application/*+json'}));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser({}));
-app.use(cors({
-  credentials: true,
-  origin: ['http://localhost:3000'],
-
-}));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(morgan('dev'));
 app.use(bodyParser.json({
@@ -31,7 +27,7 @@ app.use(upload.array());
 app.disable('x-powered-by')
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
