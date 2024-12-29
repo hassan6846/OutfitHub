fdca1fb9daa7bf89de7868a6122d3dcf7b680f25
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-12-29 12:27:38 +0500

    feat: enhance PaymentModel.js with additional fields for payment method and result

--- app/models/PaymentModel.js
+++ app/models/PaymentModel.js
@@ -10,8 +10,37 @@ const paymentSchema = new mongoose.Schema({
     PaidAt:{
         default:Date.now,
         required:false
+    },
+    PaymentMethod:{
+        type:String,
+        required:true
+    },
+    PaymentResult:{
+        id:{
+            type:String
+        },
+        status:{
+            type:String
+        },
+        update_time:{
+            type:String
+        },
+        email_address:{
+            type:String
+        }
+    }   
+    ,
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:"User",
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
     }
-})
+
+},{timestamps:true})
 
 
 module.exports = mongoose.model('Payment', paymentSchema)
