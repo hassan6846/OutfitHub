b1c02c6d63ecec0e3bd07201cb28ddac267446cc
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-12-30 17:21:18 +0500

    fix: enhance ProtectedRoute component; add cookie validation and role-based access control

--- app/client/src/components/Protected/ProductRoute.js
+++ app/client/src/components/Protected/ProductRoute.js
@@ -1,16 +1,30 @@
 import { Navigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import Cookie from "js-cookie";
+import { setAuth } from '../../Slices/UserSlices';
+import { useEffect } from 'react';
 
 const ProtectedRoute = ({ children, requiredRole }) => {
+  const dispatch = useDispatch();
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const cookie = Cookie.get("token");
   const roles = useSelector((state) => state.user.role);
 
-  if (!isAuthenticated) {
-    toast.error("You must log in to access this page or Page wont Existed");
+  // Handle the logic when the component mounts or the cookie changes
+  useEffect(() => {
+    if (!cookie) {
+      dispatch(setAuth(false));
+    }
+  }, [cookie, dispatch]); // Re-run effect when cookie value changes
+
+  // Check if the user is not authenticated or doesn't have the token
+  if (!isAuthenticated && !cookie) {
+    toast.error("You must log in to access this page or Page won't exist");
     return <Navigate to="/login" replace />;
   }
 
+  // Check if the user doesn't have the required role
   if (requiredRole && !roles.includes(requiredRole)) {
     toast.error("You do not have permission to access this page");
     return <Navigate to="/404" replace />;
